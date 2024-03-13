37b975249caefcfafa1b37d4fd3354af51c6af59
Abhi9705 <L00171125@atu.ie>
2024-03-13 10:38:39 +0000

    changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,4 +3,3 @@
 #   region = "us-east-1"
 #  key = "jenkins-server/terraform.tfstate"
 #}
-#}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -45,13 +45,32 @@ resource "aws_default_security_group" "default-sg" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+ egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    
   tags = {
     Name = "${var.env_prefix}-default-sg"
   }
